d6baefc75649ac4901161aaeb0135a378321772a
Artem <artem0fedoseev15@mail.ru>
2024-02-09 21:31:37 +0300

    Task02

---
+++ Task02/Test.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'Vector.php';
+function runTest()
+{
+    echo "String representation test" . PHP_EOL;
+    $v1 = new Vector(1, 2, 3);
+    echo "Ожидается: (1; 2; 3)" . PHP_EOL;
+    echo "Получено: " . $v1 . PHP_EOL;
+    echo PHP_EOL;
+
+    echo "Adding test" . PHP_EOL;
+    $v2 = new Vector(1, 4, -2);
+    $v3 = $v1->add($v2);
+    echo "Ожидается: (2; 6; 1)" . PHP_EOL;
+    echo "Получено: " . $v3 . PHP_EOL;
+    echo PHP_EOL;
+
+    echo "Subtract test" . PHP_EOL;
+    $v4 = $v1->sub($v2);
+    echo "Ожидается: (0; -2; 5)" . PHP_EOL;
+    echo "Получено: " . $v4 . PHP_EOL;
+    echo PHP_EOL;
+
+    echo "Product test" . PHP_EOL;
+    $v5 = $v1->prod(2);
+    echo "Ожидается: (2; 4; 6)" . PHP_EOL;
+    echo "Получено: " . $v5 . PHP_EOL;
+    echo PHP_EOL;
+
+    echo "ScalarProduct test" . PHP_EOL;
+    $v6 = $v1->scalarProd($v2);
+    echo "Ожидается: 3" . PHP_EOL;
+    echo "Получено: " . $v6 . PHP_EOL;
+    echo PHP_EOL;
+
+    echo "VectorProduct test" . PHP_EOL;
+    $v7 = $v1->vectorProd($v2);
+    echo "Ожидается: (-16; 5; 2)" . PHP_EOL;
+    echo "Получено: " . $v7 . PHP_EOL;
+    echo PHP_EOL;
+}
---
+++ Task02/Vector.php
@@ -0,0 +1,63 @@
+<?php 
+    class Vector
+    {
+        private $x;
+        private $y;
+        private $z;
+        public function __construct($x, $y, $z)
+        {
+            if (is_numeric($x) && is_numeric($y) && is_numeric($z)) {
+                $this->x = $x;
+                $this->y = $y;
+                $this->z = $z;
+            } else {
+                exit('Координаты должны быть числами.');
+            }
+        }
+
+        public function add(Vector $vector): Vector
+        {
+            $addX = $this->x + $vector->x;
+            $addY = $this->y + $vector->y;
+            $addZ = $this->z + $vector->z;
+            return new Vector($addX, $addY, $addZ);
+        }
+
+        public function sub(Vector $vector): Vector
+        {
+            $subX = $this->x - $vector->x;
+            $subY = $this->y - $vector->y;
+            $subZ = $this->z - $vector->z;
+            return new Vector($subX, $subY, $subZ);
+        }
+        
+        public function prod($number): Vector
+        {
+            $prodX = $this->x * $number;
+            $prodY = $this->y * $number;
+            $prodZ = $this->z * $number;
+            return new Vector($prodX, $prodY, $prodZ);
+        }
+
+        public function scalarProd(Vector $vector): int
+        {
+            $scalProdX = $this->x * $vector->x;
+            $scalProdY = $this->y * $vector->y;
+            $scalProdZ = $this->z * $vector->z;
+            return ($scalProdX + $scalProdY + $scalProdZ); 
+        }
+
+        public function vectorProd(Vector $vector): Vector
+        {
+            $vectProdX = $this->y * $vector->z - $this->z * $vector->y;
+            $vectProdY = $this->z * $vector->x - $this->x * $vector->z;
+            $vectProdZ = $this->x * $vector->y - $this->y * $vector->x;
+            return new Vector ($vectProdX, $vectProdY, $vectProdZ); 
+        }
+
+        public function __toString()
+        {
+            return "({$this->x}; {$this->y}; {$this->z})";
+        }
+    }
+?>
